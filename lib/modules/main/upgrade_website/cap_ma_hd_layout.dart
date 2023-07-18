c85bb0f5ab306b750510b39ebf7f917736f25d78
tuanbl <35990999+tuanbl@users.noreply.github.com>
2023-07-18 10:44:30 +0700

    Tuan update

---
+++ lib/modules/main/upgrade_website/cap_ma_hd_layout.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class CapMaHDLayout extends StatelessWidget {
+  const CapMaHDLayout() : super(key: const Key(pathName));
+
+  static const String pathName = 'cap-ma-hd';
+
+  @override
+  Widget build(BuildContext context) {
+    double hozSpacingWrap = 30;
+    double verSpacingWrap = 20;
+    String typeHD = 'web';
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Radio(
+                value: 'web', groupValue: typeHD, onChanged: (value) {
+
+              }, ),
+              Radio(
+                value: 'app', groupValue: typeHD, onChanged: (value) {
+
+              }, ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Wrap(
+                  runSpacing: 10,
+                  children: [
+                    Text('Mã hợp đồng', style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleMedium,),
+                    TextFormField(
+
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 30,),
+              Expanded(
+                child: Wrap(
+                  runSpacing: 10,
+                  children: [
+                    Text('Mã nhân viên', style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleMedium,),
+                    TextFormField(),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 30,),
+              Expanded(
+                child: Wrap(
+                  runSpacing: 10,
+                  children: [
+                    Text('Nhân viên kinh doanh', style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleMedium,),
+                    TextFormField(),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 30,),
+              Expanded(
+                child: Wrap(
+                  runSpacing: 10,
+                  children: [
+                    Text('Phòng', style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleMedium,),
+                    TextFormField(),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 30,),
+              Expanded(
+                child: Wrap(
+                  runSpacing: 10,
+                  children: [
+                    Text('Khu vực', style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleMedium,),
+                    TextFormField(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
