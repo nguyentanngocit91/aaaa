92cb6238a7f49bc3d143487a1583f95653fc0867
dangtunina <32659043+dangtunina@users.noreply.github.com>
2023-07-20 08:28:57 +0700

    update_tu_20_07
    update

--- lib/modules/main/ketoan/blacklist/screens/danh_sach_blacklist_layout.dart
+++ lib/modules/main/ketoan/blacklist/screens/danh_sach_blacklist_layout.dart
@@ -16,51 +16,145 @@ class _DanhSachBlacklistLayoutState extends State<DanhSachBlacklistLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child:Column(
+        child: Column(
           children: [
             Container(
-              width:double.maxFinite,
-              child:Text("ddd"),
-              decoration:BoxDecoration(
-                color:Color(0xfff5f5f5),
-                border:Border.all(width:1,color:Color(0xffdcdbdb))
+              padding: EdgeInsets.all(10),
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  color: Color(0xfff5f5f5),
+                  border: Border.all(width: 1, color: Color(0xffdcdbdb))),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Wrap(
+                      runSpacing: 0,
+                      children: [
+                        Text(
+                          'Mã hợp đồng',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        Container(
+                          height: 40,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.only(left: 10, right: 10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Wrap(
+                      runSpacing: 0,
+                      children: [
+                        Text(
+                          'Ghi chú',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        Container(
+                          height: 40,
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.only(left: 10, right: 10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width:10,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Wrap(
+                      runSpacing: 0,
+                      children: [
+                        Container(
+                          margin:EdgeInsets.only(top:20),
+                          child:TextButton(
+                              onPressed: () {},
+                              child: Text("Lưu thông tin",style:TextStyle(color:Colors.white),),
+                              style: ButtonStyle(
+                                backgroundColor:
+                                MaterialStatePropertyAll(Color(0xffff9800)),
+
+
+                              )),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
+            SizedBox(
+              height:30,
+            ),
             Expanded(
-              child:DataTable2(
-                  columnSpacing: 12,
-                  horizontalMargin: 12,
+              child: DataTable2(
+                  columnSpacing: 8,
+                  horizontalMargin: 8,
                   minWidth: 600,
-                  columns: const[
+                  headingRowColor: MaterialStatePropertyAll(Color(0xff105a6c)),
+                  headingTextStyle: TextStyle(color: Color(0xffffffff)),
+
+                  columns: const [
                     DataColumn2(
                       label: Text('#'),
-                      size: ColumnSize.L,
+                      fixedWidth: 50
+
                     ),
-                    DataColumn(
+                    DataColumn2(
                       label: Text('Mã HĐ'),
+                        fixedWidth:150,
+
                     ),
                     DataColumn(
                       label: Text('Ghi chú'),
                     ),
-
-                    DataColumn(
+                    DataColumn2(
                       label: Text('Thao tác'),
+                      fixedWidth:150
+                    ),
 
-                    )
                   ],
                   rows: List<DataRow>.generate(
-                      100,
-                          (index) => DataRow(cells: [
-                        DataCell(Text((index+1).toString())),
-                        DataCell(Text('A' * (10 - index % 10))),
-                        DataCell(Text('B' * (10 - (index + 5) % 10))),
+                      1000,
+                      (index) => DataRow(cells: [
+                            DataCell(Container(width:50,child:Text((index + 1).toString()),)),
+                            DataCell(Text('A11220'+index.toString())),
+                            DataCell(Text('28/6/2023 ký biên bản cho tặng source cho 1029023W CÔNG TY TNHH THƯƠNG MẠI SẢN XUẤT CAO NGUYÊN MỚI' )),
+                            DataCell(
+                              Container(
+                                width:150,
+                                color:Colors.blue,
+                                child:TextButton(
+                                  onPressed: () {
+                                    print("row" + index.toString());
+                                  },
+                                  child: Container(
+
+                                    child: Icon(Icons.close),
+                                  ),
+                                ),
+                              )
+                            ),
 
 
-
-                        DataCell( TextButton(onPressed:(){
-                          print("row"+index.toString());
-                        }, child:Container(child:Icon(Icons.close),),))
-                      ]))),
+                          ]))),
             )
           ],
         ),
@@ -68,6 +162,3 @@ class _DanhSachBlacklistLayoutState extends State<DanhSachBlacklistLayout> {
     );
   }
 }
-
-
-
