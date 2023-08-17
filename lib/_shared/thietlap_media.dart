e78ff2138d54986cccd98f31ad8baec80639f785
nguyentanngocit91 <tanit91.nina@gmail.com>
2023-08-17 10:24:24 +0700

    Delete lib directory

--- lib/_shared/thietlap_media.dart
+++
@@ -1,8 +0,0 @@
-class MediaAssets{
-  static const String assetsImages = 'assets/images';
-  static const String assetsIcons = 'assets/icons';
-  static const String assetsVideos = 'assets/videos';
-  static const String assetsSlides = 'assets/slide';
-
-  static const String logoMenuLeft = '$assetsImages/logo_nina.png';
-}
--- lib/packages/textfield_tags/textfield_tags.dart
+++
@@ -1,4 +0,0 @@
-library textfield_tags;
-
-export 'src/main.dart';
-export 'src/models.dart';
